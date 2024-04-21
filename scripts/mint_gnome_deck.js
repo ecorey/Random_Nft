7acbfc637cd2dac795c7a73a67b5b3ce7c1e229b
ecorey <bengstrom32@gmail.com>
2024-04-21 11:36:10 -0400

    ptb

---
+++ scripts/mint_gnome_deck.js
@@ -0,0 +1,85 @@
+import { getFullnodeUrl, SuiClient, SuiHTTPTransport  } from "@mysten/sui.js/client";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import walletDev from './dev-wallet.json' assert { type: 'json' };
+
+import { WebSocket } from 'ws';
+
+import {  Package, CLOCK  } from './config.js';
+
+
+// ###################################
+// ############GET CURRENT TIME#######
+// ###################################
+
+
+
+
+// generate a keypair
+const privateKeyArray = walletDev.privateKey.split(',').map(num => parseInt(num, 10));
+const privateKeyBytes = new Uint8Array(privateKeyArray);
+const keypairdev = Ed25519Keypair.fromSecretKey(privateKeyBytes);
+
+
+
+
+// client
+const client = new SuiClient({
+    transport: new SuiHTTPTransport({
+        url: getFullnodeUrl('testnet'),
+        WebSocketConstructor: WebSocket
+    }),
+});
+
+
+
+
+(async () => {
+    try {
+     
+        
+        // create Transaction Block
+        const txb = new TransactionBlock();
+
+
+        txb.setGasBudget(10000000);
+
+
+        async function mintGnomeDeck() {
+            await txb.moveCall({
+                target: `${Package}::card_deck::mint_gnome_deck`,
+                arguments: [],
+            });
+           
+        }
+
+        await mintGnomeDeck();
+        
+        
+
+
+        
+        // finalize the transaction block
+        let txid = await client.signAndExecuteTransactionBlock({
+            signer: keypairdev,
+            transactionBlock: txb,
+        });
+        
+
+
+        // // log the transaction result
+        console.log(`Transaction result: ${JSON.stringify(txid, null, 2)}`);
+        console.log(`success: https://suiexplorer.com/txblock/${txid.digest}?network=testnet`);
+
+
+
+        console.log(`END OF SCRIPT`);
+
+    } catch (e) {
+        console.error(`error: ${e}`);
+    }
+})();
+
+
+
+
