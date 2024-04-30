702490c6b4e1b38d7ee85c17d252b569dd2e23fa
ecorey <bengstrom32@gmail.com>
2024-04-30 09:09:34 -0400

    devnet random

--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,7 @@
 export const CLOCK = "0x6";
 export const RANDOM = "0x8";
 export const DEV_WALLET = "0xb7b54b78232c1f91ee518054888324dbed7db7f8143001146f68a52e28d66cce";
-export const Package = "0x58c4174c4392cc921e134c1164c1dd622f13fb085cdbfaf0842b650a911b64cb";
+export const Package = "0x40fe6d5bdd9ff26c3a1d1a4fe312114ad43920bc9c6a234025b5e8aed2338e91";
 export const MONSTI_TREASURY_CAP = "";
 
 // export const CURRENT_TIME = 1713713406533;
