edc554abe146b3b96ad1a1d3ef16c39b043fbbe1
ecorey <bengstrom32@gmail.com>
2024-05-03 14:33:30 -0400

    commit

--- capy_gnome_site/src/App.jsx
+++ capy_gnome_site/src/App.jsx
@@ -1,37 +1,46 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [readLevel, setReadLevel] = useState(0); // Initial state with 0 showing the base level text only
 
-  const [showMore, setShowMore] = useState(false);
-  
+  const content = [
+    "Leveveraging the SUI Framework Random module and a Closed-Loop Token to power gameplay, Capy Vs Gnome is a 'MTG' style game where players can battle each other using their own decks of cards till the last one is standing.", 
+    "MINT A DECK",
+    "CONFIRM A DECK",
+    "Ready to start a game? Check the chain to ensure your deck is ready to go!"
+  ];
 
   const handleReadMore = () => {
-    setShowMore(true);
-    
+    if (readLevel < content.length - 1) { // Prevents going beyond available content levels
+      setReadLevel(readLevel + 1);
+    }
   };
 
   const handleReadLess = () => {
-    setShowMore(false);
+    if (readLevel > 0) { // Prevents going below the initial text
+      setReadLevel(readLevel - 1);
+    }
   };
 
   return (
     <>
       <div className="content">
-
         <h1>CAPY Vs GNOME</h1>
-        
         <div className="text">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          {showMore && (
-            <p id="hidden">Yeah it's what you hoped for, another ...Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          )}
+          <p>{content[0]}</p>
+          {readLevel > 0 && <p>{content[1]}</p>}
+          {readLevel > 1 && <p>{content[2]}</p>}
+          {readLevel > 2 && <p>{content[3]}</p>} {/* New level added */}
         </div>
         <div className="buttons">
-          {!showMore && <button className="btn-1" onClick={handleReadMore}>Read More</button>}
-          {showMore && <button className="btn-2" onClick={handleReadLess}>Read Less</button>}
+          {readLevel > 0 && (
+            <button className="btn-2" onClick={handleReadLess}>Read Less</button>
+          )}
+          {readLevel < content.length - 1 && (
+            <button className="btn-1" onClick={handleReadMore}>Read More</button>
+          )}
         </div>
-        
       </div>
     </>
   );
