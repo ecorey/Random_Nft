8c49b97c3e197091ede03710319744ff37714ced
ecorey <bengstrom32@gmail.com>
2024-05-07 10:15:48 -0400

    commit

--- capy_gnome_site/src/components/Strip.jsx
+++ capy_gnome_site/src/components/Strip.jsx
@@ -1,4 +1,16 @@
+import React from 'react';
+import { useWallet } from '@suiet/wallet-kit';
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { Package } from '../../../scripts/config';
+
 const Strip = () => {
+
+    const { signAndExecuteTransactionBlock } = useWallet();  
+
+    
+    const handleTransferCapyDeck = () => transferCapyDeck(signAndExecuteTransactionBlock);
+    const handleTransferGnomeDeck = () => transferGnomeDeck(signAndExecuteTransactionBlock);
+
     return (
         <div style={{
             display: 'flex',
@@ -15,13 +27,62 @@ const Strip = () => {
                 flexWrap: 'wrap',
                 paddingLeft: '50px'  
             }}>  
-                <h2>MINT CAPY DECK</h2>
-                <h2>CONFIRM CAPY DECK</h2>
-                <h2>MINT GNOME DECK</h2>
-                <h2>CONFIRM GNOME DECK</h2>
+                <button style={buttonStyle} onClick={handleTransferCapyDeck}>MINT CAPY DECK</button>
+                <button style={buttonStyle}>CONFIRM CAPY DECK</button>
+                <button style={buttonStyle} onClick={handleTransferGnomeDeck}>MINT GNOME DECK</button>
+                <button style={buttonStyle}>CONFIRM GNOME DECK</button>
             </div>
         </div>
     );
 };
 
+const buttonStyle = {
+    background: 'none',
+    color: 'white',
+    border: 'none',
+    padding: '10px',
+    fontSize: '20px',
+    cursor: 'pointer',
+    outline: 'none'
+};
+
 export default Strip;
+
+
+async function transferCapyDeck(signAndExecuteTransactionBlock) {
+    const txb = new TransactionBlock();
+    txb.setGasBudget(1000000000);
+    txb.moveCall({
+        target: `${Package}::card_deck::transfer_capy_cards`,
+        arguments: [],
+    });
+
+    try {
+        const capyDeckData = await signAndExecuteTransactionBlock({
+            transactionBlock: txb
+        });
+        console.log('Capy Deck Transferred!', capyDeckData);
+        alert(`Congrats! Capy Deck Transferred! \n Digest: ${capyDeckData.digest}`);
+    } catch (e) {
+        console.error('Sorry, Capy Deck Was Not Transferred!', e);
+    }
+}
+
+async function transferGnomeDeck(signAndExecuteTransactionBlock) {
+    const txb = new TransactionBlock();
+    txb.setGasBudget(1000000000);
+    txb.moveCall({
+        target: `${Package}::card_deck::transfer_gnome_cards`,
+        arguments: [],
+    });
+
+    try {
+        const gnomeDeckData = await signAndExecuteTransactionBlock({
+            transactionBlock: txb
+        });
+        console.log('Gnome Deck Transferred!', gnomeDeckData);
+        alert(`Congrats! Gnome Deck Transferred! \n Digest: ${gnomeDeckData.digest}`);
+    } catch (e) {
+        console.error('Sorry, Gnome Deck Was Not Transferred!', e);
+    }
+}
--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,7 @@
 export const CLOCK = "0x6";
 export const RANDOM = "0x8";
 export const DEV_WALLET = "0xb7b54b78232c1f91ee518054888324dbed7db7f8143001146f68a52e28d66cce";
-export const Package = "0xe7eb1fde970c513ab332501a3a24d3801321fa2cfd06874488f9f3ced3dc09c8";
+export const Package = "0x6dff1dbc9c29caf676c88691fb651208a5a53e73e70fcb116b63ed0030bbc5ab";
 export const MONSTI_TREASURY_CAP = "";
 
 // export const CURRENT_TIME = 1713713406533;
