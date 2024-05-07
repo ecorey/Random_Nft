9ba143fd8883c80b1660df0a774933edb1e7fd94
ecorey <bengstrom32@gmail.com>
2024-05-07 19:18:46 -0400

    commit

--- capy_gnome_site/src/components/Rules.jsx
+++ capy_gnome_site/src/components/Rules.jsx
@@ -20,7 +20,7 @@ const Rules = () => {
 
 
             <p style={{fontFamily: 'pixelify sans', color: 'white', fontSize: '28px', paddingTop: '45px'}}>Permanents</p>
-            <p style={{ fontFamily: 'pixelify sans', color: 'white', fontSize: '14px' }}>
+            <p style={{ fontFamily: 'pixelify sans', color: 'white', fontSize: '16px' }}>
                 <strong>General</strong><br />
                 Attack: 10<br />
                 Defense: 2<br />
@@ -48,8 +48,33 @@ const Rules = () => {
 
 
 
-            <p style={{fontFamily: 'pixelify sans', color: 'white', fontSize: '28px', paddingTop: '45px'}}>More</p>
-            <p style={{fontFamily: 'pixelify sans', color: 'white', fontSize: '14px'}}>Some stuff about the game.</p>
+            <p style={{fontFamily: 'pixelify sans', color: 'white', fontSize: '28px', paddingTop: '45px'}}>Probabilites of Success</p>
+            <p style={{ fontFamily: 'pixelify sans', color: 'white', fontSize: '16px' }}>
+                <strong>Soldier</strong><br />
+                Soldier Vs. Soldier: 50%<br />
+                Soldier Vs. Rider: 40%<br />
+                Soldier Vs. Monster: 33%<br />
+                Soldier Vs. General: 66%<br />
+                <br />
+                <strong>Rider</strong><br />
+                Rider Vs. Soldier: 60%<br />
+                Rider Vs. Rider: 50%<br />
+                Rider Vs. Monster: 40%<br />
+                Rider Vs. General: 75%<br />
+                <br />
+                <strong>Monster</strong><br />
+                Monster Vs. Soldier: 60%<br />
+                Monster Vs. Rider: 55%<br />
+                Monster Vs. Monster: 50%<br />
+                Monster Vs. General: 80%<br />
+                <br />
+                <strong>General</strong><br />
+                General Vs. Soldier: 70%<br />
+                General Vs. Rider: 60%<br />
+                General Vs. Monster: 55%<br />
+                General Vs. General: 80%<br />
+            </p>
+
 
 
 
