0982b08b8a5b84b37d0c86a17ee202b352c46209
ecorey <bengstrom32@gmail.com>
2024-05-29 10:22:44 -0400

    commit

--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,7 @@
 export const CLOCK = "0x6";
 export const RANDOM = "0x8";
 export const DEV_WALLET = "0xb7b54b78232c1f91ee518054888324dbed7db7f8143001146f68a52e28d66cce";
-export const Package = "0xd1044fd0ca93ba47306c006a96fb646cb7ca0c01791e7bcf2c2621019cb36550";
+export const Package = "0x4bda7ea3b6989b37c6d3bac7b76664c0d9c851e94065eb8f698a28370d51f7bb";
 export const MONSTI_TREASURY_CAP = "";
 
 // export const CURRENT_TIME = 1713713406533;
